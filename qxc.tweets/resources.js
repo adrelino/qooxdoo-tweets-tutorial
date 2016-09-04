24ceb7808f9f3a16c34d03f94f6782960f836e33
Rene Jochum <rene@jochums.at>
2016-09-04 16:01:03 +0200

    Update to qx master :).
    Signed-off-by: Rene Jochum <rene@jochums.at>

--- qxc.tweets/resources.js
+++ qxc.tweets/resources.js
@@ -1,5 +1,399 @@
 qx.$$packageData['0'] = {
-  "locales": {},
+  "locales": {
+    "C": {
+      "alternateQuotationEnd": "’",
+      "alternateQuotationStart": "‘",
+      "quotationEnd": "”",
+      "quotationStart": "“",
+      "cldr_am": "AM",
+      "cldr_pm": "PM",
+      "cldr_date_format_full": "EEEE, MMMM d, y",
+      "cldr_date_format_long": "MMMM d, y",
+      "cldr_date_format_medium": "MMM d, y",
+      "cldr_date_format_short": "M/d/yy",
+      "cldr_date_time_format_Ed": "d E",
+      "cldr_date_time_format_Hm": "HH:mm",
+      "cldr_date_time_format_Hms": "HH:mm:ss",
+      "cldr_date_time_format_M": "L",
+      "cldr_date_time_format_MEd": "E, M/d",
+      "cldr_date_time_format_MMM": "LLL",
+      "cldr_date_time_format_MMMEd": "E, MMM d",
+      "cldr_date_time_format_MMMd": "MMM d",
+      "cldr_date_time_format_Md": "M/d",
+      "cldr_date_time_format_d": "d",
+      "cldr_date_time_format_hm": "h:mm a",
+      "cldr_date_time_format_hms": "h:mm:ss a",
+      "cldr_date_time_format_ms": "mm:ss",
+      "cldr_date_time_format_y": "y",
+      "cldr_date_time_format_yM": "M/y",
+      "cldr_date_time_format_yMEd": "E, M/d/y",
+      "cldr_date_time_format_yMMM": "MMM y",
+      "cldr_date_time_format_yMMMEd": "E, MMM d, y",
+      "cldr_date_time_format_yMMMd": "MMM d, y",
+      "cldr_date_time_format_yMd": "M/d/y",
+      "cldr_date_time_format_yQ": null,
+      "cldr_date_time_format_yQQQ": "QQQ y",
+      "cldr_day_format_abbreviated_fri": "Fri",
+      "cldr_day_format_abbreviated_mon": "Mon",
+      "cldr_day_format_abbreviated_sat": "Sat",
+      "cldr_day_format_abbreviated_sun": "Sun",
+      "cldr_day_format_abbreviated_thu": "Thu",
+      "cldr_day_format_abbreviated_tue": "Tue",
+      "cldr_day_format_abbreviated_wed": "Wed",
+      "cldr_day_format_wide_fri": "Friday",
+      "cldr_day_format_wide_mon": "Monday",
+      "cldr_day_format_wide_sat": "Saturday",
+      "cldr_day_format_wide_sun": "Sunday",
+      "cldr_day_format_wide_thu": "Thursday",
+      "cldr_day_format_wide_tue": "Tuesday",
+      "cldr_day_format_wide_wed": "Wednesday",
+      "cldr_day_stand-alone_narrow_fri": "F",
+      "cldr_day_stand-alone_narrow_mon": "M",
+      "cldr_day_stand-alone_narrow_sat": "S",
+      "cldr_day_stand-alone_narrow_sun": "S",
+      "cldr_day_stand-alone_narrow_thu": "T",
+      "cldr_day_stand-alone_narrow_tue": "T",
+      "cldr_day_stand-alone_narrow_wed": "W",
+      "cldr_month_format_abbreviated_1": "Jan",
+      "cldr_month_format_abbreviated_2": "Feb",
+      "cldr_month_format_abbreviated_3": "Mar",
+      "cldr_month_format_abbreviated_4": "Apr",
+      "cldr_month_format_abbreviated_5": "May",
+      "cldr_month_format_abbreviated_6": "Jun",
+      "cldr_month_format_abbreviated_7": "Jul",
+      "cldr_month_format_abbreviated_8": "Aug",
+      "cldr_month_format_abbreviated_9": "Sep",
+      "cldr_month_format_abbreviated_10": "Oct",
+      "cldr_month_format_abbreviated_11": "Nov",
+      "cldr_month_format_abbreviated_12": "Dec",
+      "cldr_month_format_wide_1": "January",
+      "cldr_month_format_wide_2": "February",
+      "cldr_month_format_wide_3": "March",
+      "cldr_month_format_wide_4": "April",
+      "cldr_month_format_wide_5": "May",
+      "cldr_month_format_wide_6": "June",
+      "cldr_month_format_wide_7": "July",
+      "cldr_month_format_wide_8": "August",
+      "cldr_month_format_wide_9": "September",
+      "cldr_month_format_wide_10": "October",
+      "cldr_month_format_wide_11": "November",
+      "cldr_month_format_wide_12": "December",
+      "cldr_month_stand-alone_narrow_1": "J",
+      "cldr_month_stand-alone_narrow_2": "F",
+      "cldr_month_stand-alone_narrow_3": "M",
+      "cldr_month_stand-alone_narrow_4": "A",
+      "cldr_month_stand-alone_narrow_5": "M",
+      "cldr_month_stand-alone_narrow_6": "J",
+      "cldr_month_stand-alone_narrow_7": "J",
+      "cldr_month_stand-alone_narrow_8": "A",
+      "cldr_month_stand-alone_narrow_9": "S",
+      "cldr_month_stand-alone_narrow_10": "O",
+      "cldr_month_stand-alone_narrow_11": "N",
+      "cldr_month_stand-alone_narrow_12": "D",
+      "cldr_number_decimal_separator": ".",
+      "cldr_number_group_separator": ",",
+      "cldr_number_percent_format": "#,##0%",
+      "day": [
+        "day"
+      ],
+      "dayperiod": [
+        "AM/PM",
+        {
+          "_": "am/pm",
+          "$": {
+            "alt": "variant"
+          }
+        }
+      ],
+      "era": [
+        "era"
+      ],
+      "hour": [
+        "hour"
+      ],
+      "minute": [
+        "minute"
+      ],
+      "month": [
+        "month"
+      ],
+      "second": [
+        "second"
+      ],
+      "week": [
+        "week"
+      ],
+      "weekday": [
+        "day of the week"
+      ],
+      "year": [
+        "year"
+      ],
+      "zone": [
+        "time zone"
+      ]
+    },
+    "de": {
+      "alternateQuotationEnd": "‘",
+      "alternateQuotationStart": "‚",
+      "quotationEnd": "“",
+      "quotationStart": "„",
+      "cldr_am": "vorm.",
+      "cldr_pm": "nachm.",
+      "cldr_date_format_full": "EEEE, d. MMMM y",
+      "cldr_date_format_long": "d. MMMM y",
+      "cldr_date_format_medium": "dd.MM.y",
+      "cldr_date_format_short": "dd.MM.yy",
+      "cldr_date_time_format_Ed": "E, d.",
+      "cldr_date_time_format_Hm": "HH:mm",
+      "cldr_date_time_format_Hms": "HH:mm:ss",
+      "cldr_date_time_format_M": "L",
+      "cldr_date_time_format_MEd": "E, d.M.",
+      "cldr_date_time_format_MMM": "LLL",
+      "cldr_date_time_format_MMMEd": "E, d. MMM",
+      "cldr_date_time_format_MMMd": "d. MMM",
+      "cldr_date_time_format_Md": "d.M.",
+      "cldr_date_time_format_d": "d",
+      "cldr_date_time_format_hm": "h:mm a",
+      "cldr_date_time_format_hms": "h:mm:ss a",
+      "cldr_date_time_format_ms": "mm:ss",
+      "cldr_date_time_format_y": "y",
+      "cldr_date_time_format_yM": "M.y",
+      "cldr_date_time_format_yMEd": "E, d.M.y",
+      "cldr_date_time_format_yMMM": "MMM y",
+      "cldr_date_time_format_yMMMEd": "E, d. MMM y",
+      "cldr_date_time_format_yMMMd": "d. MMM y",
+      "cldr_date_time_format_yMd": "d.M.y",
+      "cldr_date_time_format_yQ": null,
+      "cldr_date_time_format_yQQQ": "QQQ y",
+      "cldr_day_format_abbreviated_fri": "Fr.",
+      "cldr_day_format_abbreviated_mon": "Mo.",
+      "cldr_day_format_abbreviated_sat": "Sa.",
+      "cldr_day_format_abbreviated_sun": "So.",
+      "cldr_day_format_abbreviated_thu": "Do.",
+      "cldr_day_format_abbreviated_tue": "Di.",
+      "cldr_day_format_abbreviated_wed": "Mi.",
+      "cldr_day_format_wide_fri": "Freitag",
+      "cldr_day_format_wide_mon": "Montag",
+      "cldr_day_format_wide_sat": "Samstag",
+      "cldr_day_format_wide_sun": "Sonntag",
+      "cldr_day_format_wide_thu": "Donnerstag",
+      "cldr_day_format_wide_tue": "Dienstag",
+      "cldr_day_format_wide_wed": "Mittwoch",
+      "cldr_day_stand-alone_narrow_fri": "Fr",
+      "cldr_day_stand-alone_narrow_mon": "Mo",
+      "cldr_day_stand-alone_narrow_sat": "Sa",
+      "cldr_day_stand-alone_narrow_sun": "So",
+      "cldr_day_stand-alone_narrow_thu": "Do",
+      "cldr_day_stand-alone_narrow_tue": "Di",
+      "cldr_day_stand-alone_narrow_wed": "Mi",
+      "cldr_month_format_abbreviated_1": "Jan.",
+      "cldr_month_format_abbreviated_2": "Feb.",
+      "cldr_month_format_abbreviated_3": "März",
+      "cldr_month_format_abbreviated_4": "Apr.",
+      "cldr_month_format_abbreviated_5": "Mai",
+      "cldr_month_format_abbreviated_6": "Juni",
+      "cldr_month_format_abbreviated_7": "Juli",
+      "cldr_month_format_abbreviated_8": "Aug.",
+      "cldr_month_format_abbreviated_9": "Sep.",
+      "cldr_month_format_abbreviated_10": "Okt.",
+      "cldr_month_format_abbreviated_11": "Nov.",
+      "cldr_month_format_abbreviated_12": "Dez.",
+      "cldr_month_format_wide_1": "Januar",
+      "cldr_month_format_wide_2": "Februar",
+      "cldr_month_format_wide_3": "März",
+      "cldr_month_format_wide_4": "April",
+      "cldr_month_format_wide_5": "Mai",
+      "cldr_month_format_wide_6": "Juni",
+      "cldr_month_format_wide_7": "Juli",
+      "cldr_month_format_wide_8": "August",
+      "cldr_month_format_wide_9": "September",
+      "cldr_month_format_wide_10": "Oktober",
+      "cldr_month_format_wide_11": "November",
+      "cldr_month_format_wide_12": "Dezember",
+      "cldr_month_stand-alone_narrow_1": "Jan",
+      "cldr_month_stand-alone_narrow_2": "Feb",
+      "cldr_month_stand-alone_narrow_3": "Mär",
+      "cldr_month_stand-alone_narrow_4": "Apr",
+      "cldr_month_stand-alone_narrow_5": "Mai",
+      "cldr_month_stand-alone_narrow_6": "Jun",
+      "cldr_month_stand-alone_narrow_7": "Jul",
+      "cldr_month_stand-alone_narrow_8": "Aug",
+      "cldr_month_stand-alone_narrow_9": "Sep",
+      "cldr_month_stand-alone_narrow_10": "Okt",
+      "cldr_month_stand-alone_narrow_11": "Nov",
+      "cldr_month_stand-alone_narrow_12": "Dez",
+      "cldr_number_decimal_separator": ",",
+      "cldr_number_group_separator": ".",
+      "cldr_number_percent_format": "#,##0 %",
+      "day": [
+        "Tag"
+      ],
+      "dayperiod": [
+        "Tageshälfte"
+      ],
+      "era": [
+        "Epoche"
+      ],
+      "hour": [
+        "Stunde"
+      ],
+      "minute": [
+        "Minute"
+      ],
+      "month": [
+        "Monat"
+      ],
+      "second": [
+        "Sekunde"
+      ],
+      "week": [
+        "Woche"
+      ],
+      "weekday": [
+        "Wochentag"
+      ],
+      "year": [
+        "Jahr"
+      ],
+      "zone": [
+        "Zeitzone"
+      ]
+    },
+    "en": {
+      "alternateQuotationEnd": "’",
+      "alternateQuotationStart": "‘",
+      "quotationEnd": "”",
+      "quotationStart": "“",
+      "cldr_am": "AM",
+      "cldr_pm": "PM",
+      "cldr_date_format_full": "EEEE, MMMM d, y",
+      "cldr_date_format_long": "MMMM d, y",
+      "cldr_date_format_medium": "MMM d, y",
+      "cldr_date_format_short": "M/d/yy",
+      "cldr_date_time_format_Ed": "d E",
+      "cldr_date_time_format_Hm": "HH:mm",
+      "cldr_date_time_format_Hms": "HH:mm:ss",
+      "cldr_date_time_format_M": "L",
+      "cldr_date_time_format_MEd": "E, M/d",
+      "cldr_date_time_format_MMM": "LLL",
+      "cldr_date_time_format_MMMEd": "E, MMM d",
+      "cldr_date_time_format_MMMd": "MMM d",
+      "cldr_date_time_format_Md": "M/d",
+      "cldr_date_time_format_d": "d",
+      "cldr_date_time_format_hm": "h:mm a",
+      "cldr_date_time_format_hms": "h:mm:ss a",
+      "cldr_date_time_format_ms": "mm:ss",
+      "cldr_date_time_format_y": "y",
+      "cldr_date_time_format_yM": "M/y",
+      "cldr_date_time_format_yMEd": "E, M/d/y",
+      "cldr_date_time_format_yMMM": "MMM y",
+      "cldr_date_time_format_yMMMEd": "E, MMM d, y",
+      "cldr_date_time_format_yMMMd": "MMM d, y",
+      "cldr_date_time_format_yMd": "M/d/y",
+      "cldr_date_time_format_yQ": null,
+      "cldr_date_time_format_yQQQ": "QQQ y",
+      "cldr_day_format_abbreviated_fri": "Fri",
+      "cldr_day_format_abbreviated_mon": "Mon",
+      "cldr_day_format_abbreviated_sat": "Sat",
+      "cldr_day_format_abbreviated_sun": "Sun",
+      "cldr_day_format_abbreviated_thu": "Thu",
+      "cldr_day_format_abbreviated_tue": "Tue",
+      "cldr_day_format_abbreviated_wed": "Wed",
+      "cldr_day_format_wide_fri": "Friday",
+      "cldr_day_format_wide_mon": "Monday",
+      "cldr_day_format_wide_sat": "Saturday",
+      "cldr_day_format_wide_sun": "Sunday",
+      "cldr_day_format_wide_thu": "Thursday",
+      "cldr_day_format_wide_tue": "Tuesday",
+      "cldr_day_format_wide_wed": "Wednesday",
+      "cldr_day_stand-alone_narrow_fri": "F",
+      "cldr_day_stand-alone_narrow_mon": "M",
+      "cldr_day_stand-alone_narrow_sat": "S",
+      "cldr_day_stand-alone_narrow_sun": "S",
+      "cldr_day_stand-alone_narrow_thu": "T",
+      "cldr_day_stand-alone_narrow_tue": "T",
+      "cldr_day_stand-alone_narrow_wed": "W",
+      "cldr_month_format_abbreviated_1": "Jan",
+      "cldr_month_format_abbreviated_2": "Feb",
+      "cldr_month_format_abbreviated_3": "Mar",
+      "cldr_month_format_abbreviated_4": "Apr",
+      "cldr_month_format_abbreviated_5": "May",
+      "cldr_month_format_abbreviated_6": "Jun",
+      "cldr_month_format_abbreviated_7": "Jul",
+      "cldr_month_format_abbreviated_8": "Aug",
+      "cldr_month_format_abbreviated_9": "Sep",
+      "cldr_month_format_abbreviated_10": "Oct",
+      "cldr_month_format_abbreviated_11": "Nov",
+      "cldr_month_format_abbreviated_12": "Dec",
+      "cldr_month_format_wide_1": "January",
+      "cldr_month_format_wide_2": "February",
+      "cldr_month_format_wide_3": "March",
+      "cldr_month_format_wide_4": "April",
+      "cldr_month_format_wide_5": "May",
+      "cldr_month_format_wide_6": "June",
+      "cldr_month_format_wide_7": "July",
+      "cldr_month_format_wide_8": "August",
+      "cldr_month_format_wide_9": "September",
+      "cldr_month_format_wide_10": "October",
+      "cldr_month_format_wide_11": "November",
+      "cldr_month_format_wide_12": "December",
+      "cldr_month_stand-alone_narrow_1": "J",
+      "cldr_month_stand-alone_narrow_2": "F",
+      "cldr_month_stand-alone_narrow_3": "M",
+      "cldr_month_stand-alone_narrow_4": "A",
+      "cldr_month_stand-alone_narrow_5": "M",
+      "cldr_month_stand-alone_narrow_6": "J",
+      "cldr_month_stand-alone_narrow_7": "J",
+      "cldr_month_stand-alone_narrow_8": "A",
+      "cldr_month_stand-alone_narrow_9": "S",
+      "cldr_month_stand-alone_narrow_10": "O",
+      "cldr_month_stand-alone_narrow_11": "N",
+      "cldr_month_stand-alone_narrow_12": "D",
+      "cldr_number_decimal_separator": ".",
+      "cldr_number_group_separator": ",",
+      "cldr_number_percent_format": "#,##0%",
+      "day": [
+        "day"
+      ],
+      "dayperiod": [
+        "AM/PM",
+        {
+          "_": "am/pm",
+          "$": {
+            "alt": "variant"
+          }
+        }
+      ],
+      "era": [
+        "era"
+      ],
+      "hour": [
+        "hour"
+      ],
+      "minute": [
+        "minute"
+      ],
+      "month": [
+        "month"
+      ],
+      "second": [
+        "second"
+      ],
+      "week": [
+        "week"
+      ],
+      "weekday": [
+        "day of the week"
+      ],
+      "year": [
+        "year"
+      ],
+      "zone": [
+        "time zone"
+      ]
+    }
+  },
   "resources": {
     "qx/decoration/Simple/arrows/down-invert.gif": [
       7,
@@ -508,10 +902,6 @@ qx.$$packageData['0'] = {
   },
   "translations": {
     "C": {},
-    "en": {
-      "": "Project-Id-Version: 1.0\\nReport-Msgid-Bugs-To: you@your.org\\nPOT-Creation-Date: 2016-08-01 16:53+0100\\nPO-Revision-Date: 2016-08-01 16:53+0100\\nLast-Translator: you <you@your.org>\\nLanguage-Team: Team <yourteam@your.org>\\nMIME-Version: 1.0\\nContent-Type: text/plain; charset=utf-8\\nContent-Transfer-Encoding: 8bit\\n",
-      "$$languagename": "English"
-    },
     "de": {
       "": "Project-Id-Version: 1.0\\nReport-Msgid-Bugs-To: you@your.org\\nPOT-Creation-Date: 2016-08-01 16:54+0100\\nPO-Revision-Date: 2016-08-01 16:54+0100\\nLast-Translator: you <you@your.org>\\nLanguage-Team: Team <yourteam@your.org>\\nMIME-Version: 1.0\\nContent-Type: text/plain; charset=utf-8\\nContent-Transfer-Encoding: 8bit\\n",
       "Login": "Anmeldung",
@@ -527,6 +917,10 @@ qx.$$packageData['0'] = {
       "Post this message on identi.ca": "Stelle diese Nachricht auf identi.ca",
       "Language": "Sprache",
       "$$languagename": "German"
+    },
+    "en": {
+      "": "Project-Id-Version: 1.0\\nReport-Msgid-Bugs-To: you@your.org\\nPOT-Creation-Date: 2016-08-01 16:53+0100\\nPO-Revision-Date: 2016-08-01 16:53+0100\\nLast-Translator: you <you@your.org>\\nLanguage-Team: Team <yourteam@your.org>\\nMIME-Version: 1.0\\nContent-Type: text/plain; charset=utf-8\\nContent-Transfer-Encoding: 8bit\\n",
+      "$$languagename": "English"
     }
   }
 };
